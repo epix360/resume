0da009a67c1a8fd54ee22aee7c0e99c1270600cd
Eric Porter <eporter@scentsy.com>
2016-07-22 13:28:41 -0600

    Added caret to mobile nav

--- js/app.js
+++ js/app.js
@@ -13,13 +13,11 @@
     $('#scentsy-btn').click(function() {
         $('.entry').not('#scentsy-desc').slideUp();
         $('#scentsy-desc').stop(true, true).delay(400).toggle('blind');
-        
     });
 
     $('#bestbuy-btn').click(function() {
         $('.entry').not('#bestbuy-desc').slideUp();
         $('#bestbuy-desc').stop(true, true).delay(400).toggle('blind');
-        
     });
 
     $('#observepoint-btn').click(function() {
@@ -43,30 +41,35 @@
     $('#scentsy-btn-m').click(function() {
         $('.entry').not('#scentsy-desc').slideUp();
         $('#scentsy-desc').stop(true, true).delay(400).toggle('blind');
-        
+        $('i').not('#scentsy-btn-m i').removeClass('rotate');
+        $('#scentsy-btn-m i').toggleClass('rotate'); 
     });
 
     $('#bestbuy-btn-m').click(function() {
         $('.entry').not('#bestbuy-desc').slideUp();
         $('#bestbuy-desc').stop(true, true).delay(400).toggle('blind');
-        
+        $('i').not('#bestbuy-btn-m i').removeClass('rotate');
+        $('#bestbuy-btn-m i').toggleClass('rotate'); 
     });
 
     $('#observepoint-btn-m').click(function() {
         $('.entry').not('#observepoint-desc').slideUp();
         $('#observepoint-desc').stop(true, true).delay(400).toggle('blind');
-        
+        $('i').not('#observepoint-btn-m i').removeClass('rotate');
+        $('#observepoint-btn-m i').toggleClass('rotate'); 
     });
 
     $('#byui-btn-m').click(function() {
         $('.entry').not('#byui-desc').slideUp();
         $('#byui-desc').stop(true, true).delay(400).toggle('blind');
-        
+        $('i').not('#byui-btn-m i').removeClass('rotate');
+        $('#byui-btn-m i').toggleClass('rotate'); 
     });
 
     $('#bcw-btn-m').click(function() {
         $('.entry').not('#bcw-desc').slideUp();
-        $('#bcw-desc').stop(true, true).delay(400).toggle('blind'); 
+        $('#bcw-desc').stop(true, true).delay(400).toggle('blind');
+        $('i').not('#bcw-btn-m i').removeClass('rotate');
+        $('#bcw-btn-m i').toggleClass('rotate');
     });
-
 })();
