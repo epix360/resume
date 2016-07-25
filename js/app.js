564162f869d2f8bc51b3033c2d68787ebd9be287
Eric Porter <eporter@scentsy.com>
2016-07-25 13:51:48 -0600

    Added favicon

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
     //Set descriptions to hidden on page load
     $(document).ready(function() {
         $(".entry").hide();
-        $(".emp-logo").on('click').animationClick();
     });
     
     //Toggle content
